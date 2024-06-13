52941b7e5e685d7a4a094accbf3534cba6a07ec8
ErickInformatica <erick27997@gmail.com>
2024-06-13 10:01:04 -0600

    200 kevin

--- bot.js
+++ bot.js
@@ -381,18 +381,42 @@ const commands = {
             connection.destroy();
         });
     },
-    unmedicont: (message, args) => {
+    200: (message, args) => {
         if (!message.member.voice.channel) {
             return message.reply('Necesitas estar en un canal de voz para usar este comando.');
         }
 
         const embed = new EmbedBuilder()
-            .setTitle('AYUDAAAA UN MEDICOOOOOOOONT')
+            .setTitle('DOSCIENTOOOOOOOOOOS')
             .setColor('Aqua')
-            .setDescription(`A MIMIR ZORRA XD`)
-            .setImage('https://i.ibb.co/LZ8SbWB/kevin-zzz.png');
+            .setDescription(`DOSCIENTOOOOOOOOOOS`)
+            .setImage('https://i.ibb.co/P9yfkfV/kevinite.png');
         
         message.reply({ embeds: [embed] });
+
+        const channel = message.member.voice.channel;
+        const connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+
+        const player = createAudioPlayer();
+        const resource = createAudioResource(path.join(__dirname, '200.mp3'));
+        
+        player.play(resource);
+        connection.subscribe(player);
+
+        player.on(AudioPlayerStatus.Idle, () => {
+            player.stop();
+            connection.destroy();
+        });
+
+        player.on('error', error => {
+            console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+            player.stop();
+            connection.destroy();
+        });
     },
     bobo: (message, args) => {
         if (!message.member.voice.channel) {
